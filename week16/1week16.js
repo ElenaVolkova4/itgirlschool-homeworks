439ae3c109fc5d6e265a59b174e3ec5a934f7000
ElenaVolkova4 <tigra_v_kletke@mail.ru>
2021-08-06 00:06:35 +0300

    добавила п1 практики

---
+++ week16/1week16.js
@@ -0,0 +1,46 @@
+function sumInput() {
+    let numbers = [];
+    let sum = 0;
+    let num = 0
+    for (;;) {
+        num = +prompt('Введите число:');
+        if (!num || num === "") break;
+        sum += num
+        numbers.push(num);
+    }
+    console.log(numbers); //вывод массива (для себя)
+
+    //сортировка
+    function compareNumbers(a, b) {
+        return a - b;
+    }
+    numbers.sort(compareNumbers);
+
+    //вывод
+    alert('Введенные числа: ' + numbers);
+    alert('Результат: ' + sum);
+}
+
+
+// второй вариант
+function sumInput2() {
+    let numbers2 = [];
+    let sum2 = 0;
+
+    for (let i = 0;; i++) {
+        numbers2[i] = +prompt('Введите число:');
+        if (!numbers2[i] || numbers2[i] === "") break;
+        sum2 += numbers2[i];
+        console.log(numbers2); //вывод массива (для себя)
+    }
+
+    //сортировка
+    function compareNumbers2(a, b) {
+        return a - b;
+    }
+    numbers2.sort(compareNumbers2);
+
+    //вывод
+    alert('Введенные числа: ' + numbers2);
+    alert('Результат: ' + sum2);
+}
